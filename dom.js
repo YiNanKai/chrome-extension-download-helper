39b945fca202b8138b67addc54e246400fc678f6
justforuse <allenforuse@gmail.com>
2017-05-14 15:53:01 +0800

    first init

---
+++ dom.js
@@ -0,0 +1,29 @@
+console.log("in dom.js")
+function getLinks() {
+  var links = document.querySelectorAll("a");
+  var results = [];
+  var seenLinks = {};
+  // for (var i  = 0; i < links.length; ++i) {
+  //   var text = links[i].textContent;
+  //   if (text.length > 100)
+  //     text = text.substring(0, 100) + "...";
+  //   var link = links[i].href.replace(/(.*)#?/, "$1");
+  //   if (seenLinks[link])
+  //     continue;
+  //   seenLinks[link] = 1;
+  //   results.push({ href: link, text: text });
+  // }
+  for (var i = 0; i < links.length; i++) {
+    if (links[i].href.indexOf(".pdf") !== -1) {
+      var outer = links[i].outerHTML;
+
+      results.push(outer);
+    }
+
+  }
+  return results;
+};
+
+chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+  sendResponse(getLinks());
+});
