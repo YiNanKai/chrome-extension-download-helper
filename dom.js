46027a62e68280d37180f6fcf4dc27d27847e1a4
justforuse <allenforuse@gmail.com>
2017-05-18 22:01:12 +0800

    update

--- dom.js
+++ dom.js
@@ -1,19 +1,8 @@
-console.log("in dom.js")
 function getLinks(r, s) {
-  console.log(r, s);
   var links = document.querySelectorAll("a");
   var results = [];
   var seenLinks = {};
-  // for (var i  = 0; i < links.length; ++i) {
-  //   var text = links[i].textContent;
-  //   if (text.length > 100)
-  //     text = text.substring(0, 100) + "...";
-  //   var link = links[i].href.replace(/(.*)#?/, "$1");
-  //   if (seenLinks[link])
-  //     continue;
-  //   seenLinks[link] = 1;
-  //   results.push({ href: link, text: text });
-  // }
+  
   for (var i = 0; i < links.length; i++) {
 
     if (links[i].href.indexOf(".pdf") !== -1 || links[i].href.indexOf(".docx") !== -1 || links[i].href.indexOf(".ppt") !== -1) {
@@ -29,7 +18,6 @@ function getLinks(r, s) {
       obj.url = links[i].href;
       obj.type = type;
       obj.text = links[i].innerHTML;
-      // var outer = links[i].href;
 
       results.push(obj);
     }
@@ -39,7 +27,6 @@ function getLinks(r, s) {
 };
 
 chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
-  console.log(sender);
-  console.log(request);
+  
   sendResponse(getLinks(request, sender));
 });
